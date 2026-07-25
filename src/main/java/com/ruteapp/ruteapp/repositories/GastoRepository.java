bf2f03df6478481ef9124736af6d317ef956dc12
Angixs-zz <hpma.305@gmail.com>
2026-07-24 22:38:00 -0600

    Agregamos los primeros repositorys con la funcionalidad propia a cada uno, como para el usuario pueda encontrar el correo y valdiar que exista, asi tambien comoencontrar al organizador y si es publico el viaje, o corresponder a un gasto cuando se debe,

--- src/main/java/com/ruteapp/ruteapp/repositories/GastoRepository.java
+++ src/main/java/com/ruteapp/ruteapp/repositories/GastoRepository.java
@@ -1,5 +1,16 @@
 package com.ruteapp.ruteapp.repositories;
 
-public interface GastoRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ruteapp.ruteapp.model.Gasto;
+import com.ruteapp.ruteapp.model.Usuario;
+import com.ruteapp.ruteapp.model.Viaje;
+
+public interface GastoRepository extends JpaRepository<Gasto, Long> {
+
+    List<Gasto> findByViaje(Viaje viaje);
+
+    List<Gasto> findByPagador(Usuario pagador);
 }
--- src/main/java/com/ruteapp/ruteapp/repositories/ViajeRepository.java
+++ src/main/java/com/ruteapp/ruteapp/repositories/ViajeRepository.java
@@ -1,5 +1,15 @@
 package com.ruteapp.ruteapp.repositories;
 
-public interface ViajeRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ruteapp.ruteapp.model.Usuario;
+import com.ruteapp.ruteapp.model.Viaje;
+
+public interface ViajeRepository extends JpaRepository<Viaje, Long> {
+
+    List<Viaje> findByOrganizador(Usuario organizador);
+
+    List<Viaje> findByPublicoTrue();
 }

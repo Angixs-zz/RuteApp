1944b47ca8135e6dfe7b01c707f696479b6bbcfb
Angixs-zz <hpma.305@gmail.com>
2026-07-24 18:49:38 -0600

    EStructura inicial

---
+++ src/test/java/com/ruteapp/ruteapp/RuteappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruteapp.ruteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuteappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
